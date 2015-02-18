2adaa942bbc123bc6669f5b1ecf9161e37bf0251
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2015-02-19 01:31:33 +0200

    Add elasticsearch, remove unneeded code

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var api = require('./routes/api');
+var env = process.env.NODE_ENV || "development";
+var es = require('elasticsearch');
+var client = null;
 
 var app = express();
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,38 +18,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/api', api);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (env === 'development') {
+    client = new es.Client({
+        host: "http://localhost:9200/"
+    });
+} else {
+    client = new es.Client({
+        host: process.env.BONSAI_URL
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+app.use('/api', api);
 
 module.exports = app;
