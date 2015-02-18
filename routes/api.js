ba46878acf456b5fb747793996231c1baa101ee2
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2015-02-19 00:15:27 +0200

    Add menu API endpoints

--- routes/api.js
+++ routes/api.js
@@ -9,9 +9,34 @@ router.get('/menus', function(req, res) {
 });
 
 router.post('/menus', function(req, res) {
-    models.Menu.create({'name': req.body.name}).then(function() {
-        res.send('OK');
+    models.Menu.create({'name': req.body.name}).then(function(menu) {
+        res.json({'id': menu.id});
     });
+});
+
+router.get('/menus/:menuId', function(req, res) {
+    var menuId = req.params.menuId;
+    models.Menu.find(menuId).then(function(menu) {
+        res.json(menu);
+    });
+});
+
+router.put('/menus/:menuId', function(req,res) {
+    var menuId = req.params.menuId;
+    models.Menu.find(menuId).then(function(menu) {
+        menu.name = req.body.name;
+        menu.save().then(function() {
+            res.status(200).end();
+        });
+    });
+});
+
+router.delete('/menus/:menuId', function(req, res) {
+    var menuId = req.params.menuId;
+    models.Menu.find(menuId).then(function(menu) {
+        menu.destroy();
+    });
+    res.status(200).end();
 });
 
 module.exports = router;
