5e777287f318ffeef03efc152b240ea6f75d07c6
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2015-02-20 00:10:12 +0200

    Add item controllers

---
+++ public/controllers/item.js
@@ -0,0 +1,49 @@
+var itemControllers = angular.module('itemControllers', []);
+
+itemControllers.controller("ItemDetailController", ["$scope", "$http", "$window", "$routeParams",
+   function(scope, $http, $window, $routeParams) {
+
+    $http.get('api/menus/' + $routeParams.menuId +'/categories/' + $routeParams.categoryId + '/items/' + $routeParams.itemId).success(function(data) {
+        scope.item = data.item;
+        scope.category = data.category;
+    });
+
+    scope.delete = function(name) {
+        $http.delete('api/menus/' + $routeParams.menuId +'/categories/' + $routeParams.categoryId + '/items/' + $routeParams.itemId).success(function(){
+            $window.location.href = '#/menus/' + $routeParams.menuId;
+        });
+    };
+}]);
+
+itemControllers.controller("ItemCreateController", ["$scope", "$http", "$window", "$routeParams",
+ function(scope, $http, $window, $routeParams) {
+
+    var menuId = $routeParams.menuId;
+    var categoryId = $routeParams.categoryId;
+    $http.get('api/menus/' + menuId + '/categories/' + categoryId).success(function(data) {
+         scope.category = data.category;
+    });
+    scope.submit = function(name) {
+       $http.post('api/menus/' + menuId +'/categories/' + categoryId +'/items/add', {'name': name}).success(function(data) {
+        $window.location.href = '#/menus/' + menuId + '/categories/' + categoryId + '/items/' + data.id;
+    });
+   };
+}]);
+
+
+itemControllers.controller("ItemEditController", ["$scope", "$http", "$window", "$routeParams",
+ function(scope, $http, $window, $routeParams) {
+     var menuId = $routeParams.menuId;
+     scope.update = function() {
+         $http.get('api/menus/' + menuId + '/categories/' + $routeParams.categoryId + '/items/' + $routeParams.itemId).success(function(data) {
+            scope.category = data.category;
+            scope.item = data.item;
+        });
+     };
+     scope.update();
+     scope.submit = function(name) {
+        $http.put('api/menus/' + menuId + '/categories/' + $routeParams.categoryId + '/items/' + $routeParams.itemId, {'name': name}).success(function(data) {
+            $window.location.href =  '#/menus/' + menuId + '/categories/' + $routeParams.categoryId + '/items/' + $routeParams.itemId;
+        });
+    };
+}]);
